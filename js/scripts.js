198562cf0e2e1d1ad4168fa1e6a8a3da68449702
Maecy-s <mercy.chepkemboi@student.moringaschool.com>
2021-06-18 12:58:07 +0300

    Add function calculateDayValue called

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,15 @@ var CC, YY, MM, DD, d, dayValue;
 var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ];
 var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
 var femaleNames  = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
+
+
+function calculateDayValue(){
+    year = document.getElementById("year").value;
+    CC = parseInt(year.substring(0,2));
+    YY = parseInt(year.substring(2,4));
+    MM = parseInt(document.getElementById("month").value);
+    DD = parseInt(document.getElementById("date").value);
+    d = ( ( (CC/4) -2*CC-1) + ( (5*YY/4) ) + ((26*(MM+1)/10) ) + DD)%7;
+    console.log(d);
+    return (Math.floor(d));
+}
